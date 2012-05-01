d15133c1bb6d0faba4ea4bc8434413972f51d835
liuguangzhao <liuguangzhao@users.sf.net>
2012-05-01 19:34:26 +0000

    real usable script for ndk r7c

--- conf1.sh
+++ conf1.sh
@@ -1,5 +1,6 @@
 set -x
 . ./confn.sh
+# export CFLAGS="-g"
 export CC="$NDKHOME/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-gcc --sysroot=/opt/android-ndk/platforms/android-9/arch-arm/"
 echo $CC
 ./configure --ar=$NDKHOME/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-ar --as=$NDKHOME/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-as --ranlib=$NDK_HOME/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-ranlib --host-cc=gcc  --target=arm-linux-eabi  --disable-dvdnav --disable-dvdread --disable-dvdread-internal --disable-vcd --disable-real --disable-xanim --disable-mencoder  --disable-mp3lib  --disable-tremor-internal  --disable-fbdev --disable-tga --disable-md5sum --disable-yuv4mpeg --disable-pnm --disable-dvb --disable-tv --disable-ftp --disable-select --disable-armv6t2  --enable-mpg123 --disable-gif --disable-jpeg --enable-faad --disable-libmpeg2-internal --disable-mencoder --datadir="/sdcard/mplayer" --confdir="/sdcard/mplayer" --libdir="/sdcard/mplayer" --disable-armvfp --disable-inet6
